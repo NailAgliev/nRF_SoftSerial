351ded4eccf74915c2e1e88378a9a25f3f0dca8a
NailAgliev <nail-aglieb@bk.ru>
2017-10-27 18:53:34 +0500

    fifo not work yet

--- pca10040/blank/arm5_no_packs/softserial.c
+++ pca10040/blank/arm5_no_packs/softserial.c
@@ -1,4 +1,11 @@
 #include "softserial.h"
+#include "app_fifo.h"
+
+
+uint8_t __tx_pin = 0;
+uint8_t __rx_pin = 0;
+
+uint16_t timer_tics = 0;
 
 uint8_t index = 0;
 uint8_t rx_char = 0;
@@ -10,20 +17,41 @@ uint8_t rx_counter = 0;
 uint8_t tx_counter = 0;
 uint8_t timer_flag = 0;
 char tx_byte_for_send = 's';
+app_fifo_t rx_fifo;
+app_fifo_t tx_fifo;
 
 const nrf_drv_timer_t UART_TIMER = NRF_DRV_TIMER_INSTANCE(0);
 uint32_t err_code = NRF_SUCCESS;
 
+void SoftSerial_init(uint8_t tx_pin, uint8_t rx_pin, uint16_t baud_rate, uint8_t rx_bufer_size, uint8_t tx_bufer_size)
+{
+			
+		__tx_pin = tx_pin;
+		__rx_pin = rx_pin;
+		timer_tics = (TIMER_FREQ/(baud_rate*2));
+		timer_init();
+		soft_uart_pins_init();
+		uint8_t rx_buffer[rx_bufer_size];
+		uint8_t tx_buffer[tx_bufer_size];
+		app_fifo_init(&rx_fifo, rx_buffer, rx_bufer_size);
+		app_fifo_init(&tx_fifo, tx_buffer, tx_bufer_size);		
+}
+void SSerial_get(uint8_t * p_byte)
+{
+	app_fifo_get(&rx_fifo, p_byte);
+}
+
+void SSerial_put(uint8_t * p_tx_byte)
+{
+	app_fifo_put(&tx_fifo, *p_tx_byte);
+}
 void tx_put(void)
 {
-	tx_char = tx_byte_for_send;
 	if(timer_flag == TIMER_OFF)
 	{
-		nrf_gpio_pin_clear(SOFT_TX_PIN);
+		nrf_gpio_pin_clear(__tx_pin);
 		timer_flag = TIMER_ON_BY_TX;
-		nrf_drv_timer_enable(&UART_TIMER);
-		
-		
+		nrf_drv_timer_enable(&UART_TIMER);		
 	}
 	else
 	{
@@ -37,11 +65,11 @@ void tx_pin_set()
 	{
 		if((tx_char & ( 1 << tx_counter)) > 0)
 		{	
-			nrf_gpio_pin_set(SOFT_TX_PIN);
+			nrf_gpio_pin_set(__tx_pin);
 		}
 		else 
 		{
-			nrf_gpio_pin_clear(SOFT_TX_PIN);
+			nrf_gpio_pin_clear(__tx_pin);
 		}
 		tx_counter++;
 	}
@@ -58,7 +86,7 @@ void tx_pin_set()
 			timer_flag = TIMER_ON_BY_RX;
 		}
 		
-		nrf_gpio_pin_set(SOFT_TX_PIN);
+		nrf_gpio_pin_set(__tx_pin);
 	}	
 }
 
@@ -67,7 +95,7 @@ void rx_read()
 	if(rx_half_bit_counter > 17)   // получили 8 бит данных
 			{
 					rx_half_bit_counter = 0;
-					nrf_drv_gpiote_in_event_enable(SOFT_RX_PIN, true);// начинаем ждать стартовый бит
+					nrf_drv_gpiote_in_event_enable(__rx_pin, true);// начинаем ждать стартовый бит
 					if(timer_flag != TIMER_ON_TXRX)
 						{
 							nrf_drv_timer_disable(&UART_TIMER);
@@ -93,7 +121,7 @@ void rx_read()
 			}
 	if((rx_half_bit_counter >= 3) && ((rx_half_bit_counter % 2) == 1)) // заходим по нечетным прерываниям (8 бит данных)
 	{
-	   if(nrf_drv_gpiote_in_is_set(SOFT_RX_PIN))
+	   if(nrf_drv_gpiote_in_is_set(__rx_pin))
 		 {
 			 rx_char = rx_char | (1 << rx_counter); 
 		 }
@@ -105,12 +133,12 @@ void rx_start_bit_handler (nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t actio
 	if(timer_flag == TIMER_OFF)
 		{
 		nrf_drv_timer_enable(&UART_TIMER);
-		nrf_drv_gpiote_in_event_disable(SOFT_RX_PIN);
+		nrf_drv_gpiote_in_event_disable(__rx_pin);
 		timer_flag = TIMER_ON_BY_RX;
 		}
 		else //если таймер запущенн то не включать таймер
 	{
-		nrf_drv_gpiote_in_event_disable(SOFT_RX_PIN);
+		nrf_drv_gpiote_in_event_disable(__rx_pin);
 		timer_flag = TIMER_ON_TXRX;
 	}
 }
@@ -120,11 +148,11 @@ void soft_uart_pins_init()
 	nrf_drv_gpiote_in_config_t rx_config = GPIOTE_CONFIG_IN_SENSE_HITOLO(true);
 	rx_config.pull = NRF_GPIO_PIN_PULLUP;
 	
-	nrf_drv_gpiote_in_init (SOFT_RX_PIN, &rx_config, rx_start_bit_handler);
-	nrf_drv_gpiote_in_event_enable(SOFT_RX_PIN, true);
+	nrf_drv_gpiote_in_init (__rx_pin, &rx_config, rx_start_bit_handler);
+	nrf_drv_gpiote_in_event_enable(__rx_pin, true);
 	
-	nrf_gpio_cfg_output(SOFT_TX_PIN);
-	nrf_gpio_pin_set(SOFT_TX_PIN);
+	nrf_gpio_cfg_output(__tx_pin);
+	nrf_gpio_pin_set(__tx_pin);
 
 }
 void timer_uart_event_handler(nrf_timer_event_t event_type, void* p_context)
@@ -166,9 +194,11 @@ void timer_init(void)
     .p_context          = NULL     
 		};
 
-		err_code = nrf_drv_timer_init(&UART_TIMER, &timer_cfg, timer_uart_event_handler);
-		APP_ERROR_CHECK(err_code);
+		nrf_drv_timer_init(&UART_TIMER, &timer_cfg, timer_uart_event_handler);
 
-		nrf_drv_timer_extended_compare(&UART_TIMER, NRF_TIMER_CC_CHANNEL0, BAUDRATE_TO_TICKS(BR), NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);    
+		nrf_drv_timer_extended_compare(&UART_TIMER, NRF_TIMER_CC_CHANNEL0, timer_tics, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);    
 
 }
+
+
+
--- pca10040/blank/arm5_no_packs/softserial.h
+++ pca10040/blank/arm5_no_packs/softserial.h
@@ -10,13 +10,9 @@
 #include "app_error.h"
 #include "nrf_drv_gpiote.h"
 #include "SEGGER_RTT.h"
+#include "app_fifo.h"
 
-#define SOFT_RX_PIN		17
-#define SOFT_TX_PIN  	18
-
-
-#define BR 9600
-#define TIMER_FREQ 								16000000
+#define TIMER_FREQ 						16000000
 #define BAUDRATE_TO_TICKS(BR)   				\
 					TIMER_FREQ/(BR*2)
 					
@@ -26,12 +22,14 @@
 #define TIMER_ON_TXRX 3
 
 
-
 void timer_init();
 void rx_read();
 void soft_uart_pins_init();
 void tx_put();
-
+void SoftSerial_init(uint8_t tx_pin, uint8_t rx_pin, uint16_t baud_rate, uint8_t rx_bufer_size, uint8_t tx_bufer_size);
+void SSerial_put(uint8_t *p_tx_byte);
+void SSerial_get(uint8_t *p_byte);
+//void SSerial_tx_send_string();
 
 #endif
 
