f0af464e22644c7e8eb3c4a8cea7ca2813bf98d8
NailAgliev <nail-aglieb@bk.ru>
2018-05-07 16:05:52 +0500

    рефакторинг начало

--- main.c
+++ main.c
@@ -11,7 +11,9 @@
 
 
 static sserial_t termite;
-static sserial_t arduino;
+static sserial_t sim800;
+
+
 
 
 uint8_t byte = 0;
@@ -22,17 +24,19 @@ uint8_t n = 0;
 
 void testfunc()
 		{
-			//SSerial_get(&arduino, &byte);
-			//SSerial_put(&arduino, &byte);
-			SSerial_get_string(&arduino, test_string);
+			SSerial_get(&sim800, &byte);
+			SSerial_put(&termite, &byte);
+			SSerial_put(&sim800, &byte);
+			SSerial_get(&termite, &byte);
+			//SSerial_get_string(&arduino, test_string);
 //			n = sizeof(test_string);
 //			SEGGER_RTT_printf(0, "%d", n);
-			SSerial_put_string(&arduino, test_string);
+		//	SSerial_put_string(&arduino, test_string);
 		}
 int main(void)
 {
-		//SoftSerial_init(&termite, 17, 20, 9600, 64, 64, &testfunc);
-		SoftSerial_init(&arduino, 17, 18, 9600, 64, 64, &testfunc);
+		SoftSerial_init(&termite, 17, 20, 9600, 64, 64, &testfunc);
+		SoftSerial_init(&sim800, 17, 18, 9600, 64, 64, &testfunc);
 		while(1)
     {
 			__WFI();
